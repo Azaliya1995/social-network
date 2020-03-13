75a55f46b362a2a48e0de49f9c4c07478e93c3c9
Azaliia <azaliya.v1995@gmail.com>
2020-03-13 18:39:00 +0100

    New project

---
+++ src/Components/Profile.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img src="https://placekitten.com/1200/550"></img>
+      </div>
+      <div>ava + description</div>
+      <div>
+        My posts
+        <div>new post</div>
+        <div>Post 1</div>
+        <div>Post 1</div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
