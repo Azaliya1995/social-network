2246a1e99102c803e8c3ecda22402d20817f7eee
Azaliia <azaliya.v1995@gmail.com>
2020-04-01 18:47:04 +0200

    Refactoring.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import React from "react";
 import "./App.css";
-import Header from "./Components/Header";
-import NavBar from "./Components/Nav";
-import Profile from "./Components/Profile";
+import Header from "./Components/Header/Header";
+import NavBar from "./Components/Nav/Nav";
+import Profile from "./Components/Profile/Profile";
+import Dialogs from "./Components/Dialogs/Dialogs";
+import News from "./Components/News/News";
+import Music from "./Components/Music/Music";
+import Settings from "./Components/Settings/Settings";
+import { BrowserRouter, Route } from "react-router-dom";
 
 const App = () => {
   //функция возвращает разметку jsx
   return (
-    <div className="app-wrapper">
-      <Header />
-      <NavBar />
-      <Profile />
-    </div>
+    <BrowserRouter>
+      <div className="app-wrapper">
+        <Header />
+        <NavBar />
+        <div className="app-wrapper-content">
+          <Route exact path="/dialogs" render={ () => <Dialogs /> } />
+          <Route path="/profile" render={ () => <Profile />} />
+
+          <Route path="/news" component={News} />
+          <Route path="/music" component={Music} />
+          <Route path="/settings" component={Settings} />
+        </div>
+      </div>
+    </BrowserRouter>
   );
 };
 
